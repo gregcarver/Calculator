39a847b6f1d257d5673656f325c841ce11a8b3ef
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-06-29 16:50:36 -0500

    numbers is good like

--- scripts.js
+++ scripts.js
@@ -1,21 +1,59 @@
 document.addEventListener('DOMContentLoaded',function(){
     console.log('im ready')
+})
+
+
+//number value on click
+
+    $('.number').on('click',function(){
+    var buttonText=$(this).text()
+    $('#readout').append(buttonText)
+    })
+
 
 //highlight
-
-})
 $('.button').on('mousedown',function(){
     $(this).addClass('red')
 })
 $('.button').on('mouseup',function(){
     $(this).removeClass('red')
 })
+//clear button
+
+$('#clear').on('click',function(){
+    $('.result').empty()
+})
+
+
+//add button
+$('#add').on('click',function(){
+    
+})
+
+//subtract button
+$('#subtract').on('click',function(){
+    //something
+})
+
+//divide
+$('#divide').on('click',function(){
+   //something
+})
+
+//multiply
+$('#multiply').on('click',function(){
+   //something
+})
+
+//equals
+$('#result').on('click',function(){
+  // $('#readout').text('hi')
+})
+
+
+
+
+//when you press =
 
 
 //function that takes the value (up to two numbers) of button clicked
-
-$('.result').text('hi')
-
-//math
-
-//print result
