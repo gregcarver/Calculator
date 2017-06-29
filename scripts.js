b8a939cf5a3b6a4969da2eb7444aff3fa9bce4fb
Innovationbham_2 <Innovationbham_2@UABiLab-2s-iMac.local>
2017-06-29 14:52:42 -0500

    framed calculator

---
+++ scripts.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded',function(){
+    console.log('im ready')
+
+//highlight
+
+})
+$('.button').on('mousedown',function(){
+    $(this).addClass('red')
+})
+$('.button').on('mouseup',function(){
+    $(this).removeClass('red')
+})
+
+
+//function that takes the value (up to two numbers) of button clicked
+
+$('.result').text('hi')
+
+//math
+
+//print result
